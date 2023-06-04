b25a9f39cfcec6b5c0b50c4b0fd75ff073b1aef9
dieggspapu <alo20172@uvg.edu.gt>
2023-06-04 17:54:50 -0600

    component: navbar with stories,css and jsx

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './navbar.module.css'
+
+export default function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      hola
+    </nav>
+  )
+}
---
+++ src/components/navbar/navbar.stories.jsx
@@ -0,0 +1,14 @@
+import NavBar from './navbar'
+
+export default {
+  title: 'Example/NavBar',
+  component: NavBar,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+}
+
+export const ColorAndBackground = {
+  args: {
+  },
+}
