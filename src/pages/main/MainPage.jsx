19e25c0dc0c608b2386c10acbc48aa525f37c657
dieggspapu <alo20172@uvg.edu.gt>
2023-06-05 20:54:15 -0600

    Thing

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -17,30 +17,39 @@ export default function MainPage() {
       <div className={styles.imageBox}>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/lab3"><img src={Lab3} alt="Labs" /></a>
+          <p>Lab: handling html</p>
         </div>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/lab4"><img src={Lab4} alt="Labs" /></a>
+          <p>Lab: learning css</p>
         </div>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/lab5"><img src={Lab5} alt="Labs" /></a>
+          <p>Lab: learning js</p>
         </div>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/lab6"><img src={Lab6} alt="Labs" /></a>
+          <p>Lab: react</p>
         </div>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/lab7"><img src={Lab7} alt="Labs" /></a>
+          <p>Lab: eslint</p>
         </div>
         <div className={styles.image}>
           <a href="https://64609611446d775c8be23587--cerulean-sable-b9711b.netlify.app/"><img src={Lab8} alt="Labs" /></a>
+          <p>Lab: global states</p>
         </div>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/lab9"><img src={Lab9} alt="Labs" /></a>
+          <p>Lab: testing</p>
         </div>
         <div className={styles.image}>
           <a href="https://uvgenios.online/20172/project1-web"><img src={Project1} alt="Labs" /></a>
+          <p>Project: Disney+</p>
         </div>
         <div className={styles.image}>
           <a href="https://deluxe-taiyaki-4e7e67.netlify.app"><img src={Project2} alt="Labs" /></a>
+          <p>Project: learning angular</p>
         </div>
       </div>
     </div>
