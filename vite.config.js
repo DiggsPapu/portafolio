68394e235e82b1ccfcdc8d320c4f03511191d8eb
DiggsPapu <diggs.papu@gmail.com>
2025-05-18 12:59:07 -0700

    feat: apply the resolve path in vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portafolio/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Example: '@' points to 'src' folder
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
   test: {
     setupFiles: './src/setUpTest.js',
     globals: true,
